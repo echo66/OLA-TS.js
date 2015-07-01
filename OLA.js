af2df27f307a4027205038d5860c2d50a45a9117
Bruno Dias <bruno.filipe.silva.dias@gmail.com>
2015-07-01 17:06:41 +0100

    Added several windowing functions.

--- OLA.js
+++ OLA.js
@@ -1,4 +1,4 @@
-function OLA(frameSize, hop) {
+function OLATS(frameSize, windowType) {
 
   this.process = function(frame) {
 
@@ -40,12 +40,15 @@ function OLA(frameSize, hop) {
     return aux;
   }
 
-  function create_sin_beta_window(length, beta) {
+  function create_window(length, beta, type) {
 
     var win = new Float32Array(length);
 
-    for (var i=0; i<length; i++)
-      win[i] = Math.pow(Math.sin(Math.PI*i/length), beta);
+    // for (var i=0; i<length; i++)
+    //   win[i] = Math.pow(Math.sin(Math.PI*i/length), beta);
+    for (var i=0; i<length; i++) {
+      win[i] = WindowFunctions[type](length, i, beta);
+    }
 
     return win;
 
@@ -69,8 +72,6 @@ function OLA(frameSize, hop) {
 
   }
 
-
-
   function create_constant_array(size, constant, ArrayType) {
     var arr = new ((ArrayType)?ArrayType:Array)(size);
     for (var i=0; i<size; i++)
@@ -78,15 +79,76 @@ function OLA(frameSize, hop) {
     return arr;
   }
 
+  this.set_beta = function(newBeta) {
+    _beta = newBeta;
+  }
+
+  this.set_overlap = function(newOverlap) {
+    _overlapFactor = newOverlap;
+  }
+
+  var WindowFunctions = {
+
+    SinBeta : function(length, index, beta) {
+      return Math.pow(Math.sin(Math.PI*index/length), beta)
+    },
+
+    Bartlett : function(length, index) {
+      return 2 / (length - 1) * ((length - 1) / 2 - Math.abs(index - (length - 1) / 2));
+    },
+
+    BartlettHann : function(length, index) {
+      return 0.62 - 0.48 * Math.abs(index / (length - 1) - 0.5) - 0.38 * Math.cos(2 * Math.PI * index / (length - 1));
+    },
+
+    Blackman : function(length, index, alpha) {
+      var a0 = (1 - alpha) / 2;
+      var a1 = 0.5;
+      var a2 = alpha / 2;
+
+      return a0 - a1 * Math.cos(2 * Math.PI * index / (length - 1)) + a2 * Math.cos(4 * Math.PI * index / (length - 1));
+    },
+
+    Cosine : function(length, index) {
+      return Math.cos(Math.PI * index / (length - 1) - Math.PI / 2);
+    },
+
+    Gauss : function(length, index, alpha) {
+      return Math.pow(Math.E, -0.5 * Math.pow((index - (length - 1) / 2) / (alpha * (length - 1) / 2), 2));
+    },
+
+    Hamming : function(length, index) {
+      return 0.54 - 0.46 * Math.cos(2 * Math.PI * index / (length - 1));
+    },
+
+    Hann : function(length, index) {
+      return 0.5 * (1 - Math.cos(2 * Math.PI * index / (length - 1)));
+    },
+
+    Lanczos : function(length, index) {
+      var x = 2 * index / (length - 1) - 1;
+      return Math.sin(Math.PI * x) / (Math.PI * x);
+    },
+
+    Rectangular : function(length, index) {
+      return 1;
+    },
+
+    Triangular : function(length, index) {
+      return 2 / length * (length / 2 - Math.abs(index - (length - 1) / 2));
+    }
+  };
+
 
   var _frameSize = frameSize;
   var _alpha, _RA, _RS;
-  var _overlapFactor = 2.6;
+  var _beta = 15;
+  var _overlapFactor = 2.3;
 
   this.set_alpha(1);
 
   var _midBuffer = new Float32Array(_frameSize - _RS);
-  var _window = create_sin_beta_window(frameSize, 4);
+  var _window = create_window(frameSize, _beta, "Lanczos");
 
   var _squaredFramingWindow = new Float32Array(_window.length);
   for (var i=0; i<_squaredFramingWindow.length; i++) {
@@ -97,6 +159,9 @@ function OLA(frameSize, hop) {
   var _owOverlapBuffers = create_constant_array(_frameSize, 0, Array);
 
 
+
+
+  
   
 
 }
