db61b98518c07af4c0eec906d64f3f8883c483a5
echo66 <bruno.filipe.silva.dias@gmail.com>
2015-10-19 17:12:53 +0100

    Fixed some issues with BufferedTS.

--- buffered-ts.js
+++ buffered-ts.js
@@ -1,3 +1,12 @@
+/*
+ *	A helper class to use OLATS with the Web Audio API.
+ *	Just pass an AudioBuffer with the "set_audio_buffer" method.
+ *	Then, for example, at each cycle of ScriptProcessor.onaudioprocess, 
+ *	change the "alpha" and "position" fields to change the stretching 
+ *  factor and the audio buffer position pointer. After changing one 
+ *  or both parameters, call the "process" method.
+ *  
+ */
 function BufferedTS(frameSize) {
 
 	var _frameSize = frameSize || 4096;
@@ -5,10 +14,11 @@ function BufferedTS(frameSize) {
 	var _olaR = new OLATS(_frameSize);
 	var _buffer;
 	var _position = 0;
-	var _alpha = 1;
+	var _newAlpha = 1;
 
 	var _midBufL = new CBuffer(Math.round(_frameSize * 1.2));
 	var _midBufR = new CBuffer(Math.round(_frameSize * 1.2));
+	var _zeros = new Float32Array(frameSize);
 
 	
 
@@ -19,46 +29,54 @@ function BufferedTS(frameSize) {
 
 		var sampleCounter = 0;
 
-        var il = _buffer.getChannelData(0);
-        var ir = _buffer.getChannelData(0);
-        var ol = outputAudioBuffer.getChannelData(0);
-        var or = outputAudioBuffer.getChannelData(1);
+		var il = _buffer.getChannelData(0);
+		var ir = _buffer.getChannelData(0);
+		var ol = outputAudioBuffer.getChannelData(0);
+		var or = outputAudioBuffer.getChannelData(1);
 
 
-        while (_midBufR.size > 0 && sampleCounter < outputAudioBuffer.length) {
-          var i = sampleCounter++;
-          ol[i] = _midBufL.shift();
-          or[i] = _midBufR.shift();
-        }
+		while (_midBufR.size > 0 && sampleCounter < outputAudioBuffer.length) {
+			var i = sampleCounter++;
+			ol[i] = _midBufL.shift();
+			or[i] = _midBufR.shift();
+		}
 
-        if (sampleCounter >= _frameSize)
-          return;
+		if (sampleCounter == outputAudioBuffer.length)
+			return;
 
-        do {
+		do {
 
-          var bufL = il.subarray(_position, _position + _frameSize);
-          var bufR = ir.subarray(_position, _position + _frameSize);
+			var frameL = il.subarray(_position, _position + _frameSize);
+			var frameR = ir.subarray(_position, _position + _frameSize);
 
-          if (_alpha) {
-            _olaL.set_alpha(_alpha, overlap, beta);
-            _olaR.set_alpha(_alpha, overlap, beta);
-          }
+			if (_newAlpha != undefined && _newAlpha != _olaL.get_alpha()) {
+				_olaL.set_alpha(_newAlpha);
+				_olaR.set_alpha(_newAlpha);
+				_newAlpha = undefined;
+			}
 
+			if (frameL.length < _frameSize) {
+				_zeros.set(0, frameL);
+				_olaL.process(_zeros, _midBufL);
+				_zeros.set(0, frameR);
+				_olaR.process(_zeros, _midBufR);
+				_zeros.fill(0);
+			} else {
+				_olaL.process(frameL, _midBufL);
+				_olaR.process(frameR, _midBufR);
+			}
+			
+			for (var i=sampleCounter; _midBufL.size > 0 && i < outputAudioBuffer.length; i++) {
+				ol[i] = _midBufL.shift();
+				or[i] = _midBufR.shift();
+			}
 
-          /* LEFT */
-          _olaL.process(bufL, _midBufL);
-          _olaR.process(bufR, _midBufR);
-          for (var i=sampleCounter; _midBufL.size > 0 && i < outputAudioBuffer.length; i++) {
-            ol[i] = _midBufL.shift();
-            or[i] = _midBufR.shift();
-          }
+			sampleCounter += _olaL.get_hs();
 
-          sampleCounter += _olaL.get_rs();
+			_position
+			+= _olaL.get_ha();
 
-          _position
-           += _olaL.get_ra();
-
-        } while (sampleCounter < outputAudioBuffer.length);
+		} while (sampleCounter < outputAudioBuffer.length);
 	}
 
 	this.set_audio_buffer = function(newBuffer) {
@@ -71,15 +89,15 @@ function BufferedTS(frameSize) {
 				return _position;
 			}, 
 			set : function(newPosition) {
-				_position = newPosition;
+				_position = new Number(newPosition);
 			}
 		}, 
 		'alpha' : {
 			get : function() {
-				return _alpha;
+				return _olaL.get_alpha();
 			}, 
 			set : function(newAlpha) {
-				_alpha = newAlpha;
+				_newAlpha = new Number(newAlpha);
 			}
 		}
 	});
